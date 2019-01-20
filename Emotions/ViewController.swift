a1ebccd89c4b77003fe86cf2b7387c47114e1476
Zachary Hardin <zbhardin@gmail.com>
2019-01-20 15:59:36 -0600

    Initial Commit

---
+++ Emotions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emotions
+//
+//  Created by Zachary Hardin on 1/20/19.
+//  Copyright © 2019 Zachary Hardin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EmotionsTests/EmotionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  EmotionsTests.swift
+//  EmotionsTests
+//
+//  Created by Zachary Hardin on 1/20/19.
+//  Copyright © 2019 Zachary Hardin. All rights reserved.
+//
+
+import XCTest
+@testable import Emotions
+
+class EmotionsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
